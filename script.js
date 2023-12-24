72bcef7fddb4bba17d65630df95e64f2d2f1bf9d
Smera <smera@berkeley.edu>
2023-12-24 11:01:21 -0800

    changed

--- script.js
+++ script.js
@@ -11,16 +11,21 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // Function to initialize the pixel grid
     function initializePixelGrid(num) {
-        const pixelGrid = document.querySelector('.grid'); // Update this based on your HTML structure
-        pixelGrid.style.maxWidth = '200px';  // Set max width for the entire grid
-        pixelGrid.style.maxHeight = '200px'; // Set max height for the entire grid
-        
+        const pixelGrid = document.querySelector('.grid');
+        pixelGrid.innerHTML = ''; // Clear existing content
+    
+        const gridSize = 300;
+        const pixelSize = gridSize / num;
+    
         for (let i = 0; i < num; i++) {
             const row = document.createElement('div');
             row.className = 'row';
-        
+    
             for (let j = 0; j < num; j++) {
-                const square = createSquare();
+                const square = document.createElement('div');
+                square.className = 'pixel';
+                square.style.width = pixelSize + 'px';
+                square.style.height = pixelSize + 'px';
                 row.appendChild(square);
             }
     
@@ -44,7 +49,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const changeButton = document.getElementsByClassName('new');
 
     function changeGrid() {
-        num = prompt("Enter a number between 1 and 64");
+        // num = prompt("Enter a number between 1 and 64");
+        num = document.getElementById('inputText').value;
         initializePixelGrid(num)};
 
 
